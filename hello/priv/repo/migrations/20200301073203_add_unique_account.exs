0325a0f258c8bad5731121a92780bbebbd9d18ea
Huy Nguyen <huynguyenxuan0697@gmail.com>
2020-03-04 15:30:01 +0700

    add resp error list

--- hello/priv/repo/migrations/20200301073203_add_unique_account.exs
+++ hello/priv/repo/migrations/20200301073203_add_unique_account.exs
@@ -2,7 +2,7 @@ defmodule Hello.Repo.Migrations.AddUniqueAccount do
   use Ecto.Migration
 
   def change do
-    create unique_index(:users, [:account, :money])
+    create unique_index(:users, [:account])
     create constraint("users", :money_must_be_positive, check: "money >= 0")
   end
 end
